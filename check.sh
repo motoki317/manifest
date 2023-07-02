36854984027b1dc8e23cacf50cfce9610c2699a7
motoki317 <motoki317@gmail.com>
2023-07-02 21:46:23 +0900

    add actions validation (#1)

---
+++ check.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+set -euxo pipefail
+
+contains () {
+  local e match="$1"
+  shift
+  for e; do [[ "$e" == "$match" ]] && return 0; done
+  return 1
+}
+
+skip_dirs=("auth-template")
+for directory in $(echo ./*/ | tr -d './' | tr -d '/'); do
+  if ! contains "$directory" "${skip_dirs[@]}"; then
+    kubectl create namespace "$directory" --dry-run=client -o yaml | kubectl apply -f -
+  fi
+done
+for directory in $(echo ./*/ | tr -d './' | tr -d '/'); do
+  if ! contains "$directory" "${skip_dirs[@]}"; then
+    kustomize build ./"$directory" --enable-alpha-plugins --enable-exec | kubectl apply --validate=strict --dry-run=server -f -
+  fi
+done

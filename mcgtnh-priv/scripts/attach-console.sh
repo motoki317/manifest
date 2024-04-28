1585a9a2f2a0d306248d4559dc08a84554e8914b
motoki317 <motoki317@gmail.com>
2024-04-28 13:59:31 +0900

    Move arg position

--- mcgtnh-priv/scripts/attach-console.sh
+++ mcgtnh-priv/scripts/attach-console.sh
@@ -4,4 +4,4 @@ echo "Attaching to server console ..."
 echo "To detach, type the escape sequence Ctrl+P followed by Ctrl+Q."
 echo "For more, see https://kubernetes.io/docs/reference/kubectl/docker-cli-to-kubectl/#docker-attach."
 
-exec kubectl attach mcgtnh-priv-0 -it
+exec kubectl attach -it mcgtnh-priv-0

7c9b738dfa062edddbb08b06ea264128a03e4cc4
motoki317 <motoki317@gmail.com>
2024-05-31 10:12:54 +0900

    fix: arm64

--- dev-ops-bot/config/entrypoint.sh
+++ dev-ops-bot/config/entrypoint.sh
@@ -8,7 +8,7 @@ apk add --no-cache git openssh curl npm
 
 # Install kubectl
 echo "Installing kubectl ..."
-curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/arm64/kubectl"
 install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 kubectl version --client
 

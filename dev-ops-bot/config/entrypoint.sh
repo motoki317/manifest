02143b20607d5edda48d6c4391f2a30931bec79b
motoki317 <motoki317@gmail.com>
2024-07-03 10:15:00 +0900

    Unnecessary setup?

--- dev-ops-bot/config/entrypoint.sh
+++ dev-ops-bot/config/entrypoint.sh
@@ -12,14 +12,4 @@ curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stabl
 install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 kubectl version --client
 
-# Set up kubectl context
-# Use mounted service account token for connecting local cluster
-# ref: https://www.ibm.com/docs/en/cloud-private/3.2.x?topic=kubectl-using-service-account-tokens-connect-api-server
-echo "Configuring kubectl to use service account token ..."
-kubectl config set-cluster cfc --server=https://kubernetes.default --certificate-authority=/var/run/secrets/kubernetes.io/serviceaccount/ca.crt
-kubectl config set-context cfc --cluster=cfc
-kubectl config set-credentials user --token="$(cat /var/run/secrets/kubernetes.io/serviceaccount/token)"
-kubectl config set-context cfc --user=user
-kubectl config use-context cfc
-
 exec /work/dev-ops-bot "$@"

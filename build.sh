5ed43b5b0d798a4d23551a5a699e038b5080ca01
motoki317 <motoki317@gmail.com>
2024-10-10 14:38:00 +0900

    Refactor validate

---
+++ build.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+contains () {
+  local e match="$1"
+  shift
+  for e; do [[ "$e" == "$match" ]] && return 0; done
+  return 1
+}
+
+rm -rf .built
+mkdir .built
+
+skip_dirs=()
+for directory in $(echo ./*/ | tr -d './' | tr -d '/'); do
+  if contains "$directory" "${skip_dirs[@]}"; then
+    echo "Skipping ./$directory"
+  else
+    echo "Building ./$directory"
+    kustomize build ./"$directory" --enable-alpha-plugins --enable-exec --enable-helm \
+      > .built/"$directory".yaml
+  fi
+done
--- check.sh
+++ check.sh
@@ -1,21 +1,6 @@
 #!/usr/bin/env bash
 
-set -euxo pipefail
-
-contains () {
-  local e match="$1"
-  shift
-  for e; do [[ "$e" == "$match" ]] && return 0; done
-  return 1
-}
-
-skip_dirs=()
-for directory in $(echo ./*/ | tr -d './' | tr -d '/'); do
-  if ! contains "$directory" "${skip_dirs[@]}"; then
-    kustomize build ./"$directory" --enable-alpha-plugins --enable-exec --enable-helm | \
-      kubeconform \
-       -schema-location default \
-       -schema-location '.crd/{{ .Group }}-{{ .ResourceKind }}-{{ .ResourceAPIVersion }}.json' \
-       -skip apiextensions.k8s.io/v1/CustomResourceDefinition
-  fi
-done
+exec kubeconform \
+  -schema-location default \
+  -schema-location '.crd/{{ .Group }}-{{ .ResourceKind }}-{{ .ResourceAPIVersion }}.json' \
+  -skip apiextensions.k8s.io/v1/CustomResourceDefinition

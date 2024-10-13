9d06bf85a32928f98108e950f01558f2666437f2
motoki317 <motoki317@gmail.com>
2024-10-13 14:31:05 +0900

    refactor/fix: fix harbor diff

---
+++ setup-crd.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+mkdir -p .crd
+cd .crd || exit
+
+# renovate:github-url
+wget https://raw.githubusercontent.com/yannh/kubeconform/v0.6.7/scripts/openapi2jsonschema.py
+export FILENAME_FORMAT='{fullgroup}-{kind}-{version}'
+
+# renovate:github-url
+python3 openapi2jsonschema.py https://raw.githubusercontent.com/argoproj/argo-cd/v2.12.4/manifests/install.yaml
+
+# renovate:github-url
+python3 openapi2jsonschema.py https://github.com/argoproj/argo-rollouts/releases/download/v1.7.2/install.yaml
+
+# NOTE: In Argo Workflows, install.yaml contains only minimal CRDs; use full CRDs for validation:
+# https://github.com/argoproj/argo-workflows/issues/11266
+# renovate:github-url
+kustomize build https://github.com/argoproj/argo-workflows//manifests/base/crds/full?ref=v3.5.11 > crd.yaml
+python3 openapi2jsonschema.py crd.yaml && rm crd.yaml
+
+# renovate:github-url
+python3 openapi2jsonschema.py https://raw.githubusercontent.com/traefik/traefik/v3.1.6/docs/content/reference/dynamic-configuration/kubernetes-crd-definition-v1.yml
+
+# renovate:github-url
+python3 openapi2jsonschema.py https://github.com/cert-manager/cert-manager/releases/download/v1.16.1/cert-manager.yaml
+
+# renovate:github-url
+python3 openapi2jsonschema.py https://github.com/rancher/system-upgrade-controller/releases/download/v0.14.1/crd.yaml

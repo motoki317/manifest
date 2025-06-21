d8472f2309a9316144162ada5d7fa88adc09d88e
motoki317 <motoki317@gmail.com>
2025-06-21 21:17:57 +0900

    allow specifying dir

--- build.sh
+++ build.sh
@@ -2,6 +2,11 @@
 
 set -euo pipefail
 
+TARGET=""
+if [ "$#" -ge 1 ]; then
+  TARGET=$1
+fi
+
 contains () {
   local e match="$1"
   shift
@@ -15,11 +20,15 @@ mkdir .built
 skip_dirs=()
 for directory in $(echo ./*/ | tr -d './' | tr -d '/'); do
   if contains "$directory" "${skip_dirs[@]}"; then
-    echo "Skipping ./$directory"
-  else
-    echo "Building ./$directory"
-    kustomize build ./"$directory" --enable-alpha-plugins --enable-exec --load-restrictor LoadRestrictionsNone --enable-helm \
-      | yq ".metadata.namespace = (.metadata.namespace // \"$directory\")" \
-      > .built/"$directory".yaml
+    echo "Skipping $directory"
+    continue
   fi
+  if [ -n "$TARGET" ] && [ "$TARGET" != "$directory" ]; then
+    continue
+  fi
+
+  echo "Building ./$directory"
+  kustomize build ./"$directory" --enable-alpha-plugins --enable-exec --load-restrictor LoadRestrictionsNone --enable-helm \
+    | yq ".metadata.namespace = (.metadata.namespace // \"$directory\")" \
+    > .built/"$directory".yaml
 done

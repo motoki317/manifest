7a2cb6ae4d755246277d255a07bab7aef4c81a05
motoki317 <motoki317@gmail.com>
2025-08-30 11:44:59 +0900

    fix config warnings

--- .github/renovate-bot.js
+++ .github/renovate-bot.js
@@ -2,6 +2,7 @@ module.exports = {
   "$schema": "https://docs.renovatebot.com/renovate-schema.json",
   "username": "moto-renovate[bot]",
   "gitAuthor": "moto-renovate <138499486+moto-renovate[bot]@users.noreply.github.com>",
+  "platform": "github",
   "repositories": ["motoki317/manifest"],
   "hostRules": [
     {
